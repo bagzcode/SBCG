7b2c310463635468170d9d68cae3398f3f32d31b
Bagus Manuaba <bagus.manuaba@gmail.com>
2017-11-23 16:54:08 +0700

    updating main scene with new assets

---
+++ Assets/Script/Tab.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Data;
+using Mono.Data.SqliteClient;
+using UnityEngine.SceneManagement;
+
+public class Tab : MonoBehaviour {
+
+
+	public GameObject PC;
+	public GameObject F;
+	public GameObject RM;
+	public GameObject P;
+
+	void Start()
+	{
+		PC = GameObject.Find("PC");
+		F = GameObject.Find("F");
+		RM = GameObject.Find("RM");
+		P = GameObject.Find("P");
+	}
+
+	public void tabPC ()
+	{
+		
+		PC.SetActive(true);
+		F.SetActive(false);
+		RM.SetActive(false);
+		P.SetActive(false);
+	}
+
+	public void tabF ()
+	{
+		
+		PC.SetActive(false);
+		F.SetActive(true);
+		RM.SetActive(false);
+		P.SetActive(false);
+	}
+
+	public void tabRM ()
+	{
+		
+		PC.SetActive(false);
+		F.SetActive(false);
+		RM.SetActive(true);
+		P.SetActive(false);
+	}
+
+	public void tabP ()
+	{
+		
+		PC.SetActive(false);
+		F.SetActive(false);
+		RM.SetActive(false);
+		P.SetActive(true);
+	}
+
+	
+
+	
+}
